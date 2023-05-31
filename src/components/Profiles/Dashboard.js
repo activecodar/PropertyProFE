c93790496972ecb08084582c8f49213f0d624cd6
Maina Kamau <lewiikamaa8@gmail.com>
2023-05-31 06:20:23 +0300

    create layout of the dashboard with static content
    Signed-off-by: Maina Kamau <lewiikamaa8@gmail.com>

--- src/components/Profiles/Dashboard.js
+++ src/components/Profiles/Dashboard.js
@@ -1,10 +1,17 @@
 import React from "react";
+import Sidebar from "./Sidebar";
+import Navbar from "./Navbar";
+import DashboardContent from "./DashboardContent";
 
 const Dashboard = () => {
     return (
-        <div>
-            <h1>Dashboard</h1>
-        </div>
+        <>
+            <Sidebar />
+            <div className="ml-auto mb-6 lg:w-[75%] xl:w-[80%] 2xl:w-[85%]">
+                <Navbar />
+                <DashboardContent />
+            </div>
+        </>
     );
 }
 export default Dashboard;
